2f92be5293d9206be7f38465fccd8d3e0aca6595
yogita <ysagarwal1953@gmail.com>
2025-06-28 02:45:52 +0000

    iit again

---
+++ rudra_common/rudra_common/doctype/test_master/test_master.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, yogi and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class TestMaster(Document):
+	pass
