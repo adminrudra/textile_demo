f0fc202b85e46eb3ebfa002a144f0b13f3df62b0
Yogita <ysagarwal1953@gmail.com>
2025-07-01 15:58:46 +0000

    added requried doctypes

---
+++ rudra_common/rudra_common/doctype/item_grp/test_item_grp.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, yogi and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestItemGrp(FrappeTestCase):
+	pass
---
+++ rudra_common/rudra_common/doctype/item_sub_group/test_item_sub_group.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, yogi and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestItemSubGroup(FrappeTestCase):
+	pass
